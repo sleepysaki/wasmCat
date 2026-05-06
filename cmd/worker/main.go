93ad315eef5a938b4a022e2b534949cb27081e87
sleepysaki_ <caotrankhanhlinh35@gmail.com>
2026-05-07 02:33:55 +0700

    add worker main.go and error handler for server.go

---
+++ cmd/worker/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"wasmcat/internal/worker"
+)
+
+func main() {
+	// empty context that can be used to set timeouts, cancel fnc, etc
+	// := is a shorthand for declaring and initializing a variable in one line -> create new variable called ctx and assign it the value of context.Background()
+	ctx := context.Background()
+
+	engine := worker.newWasmEngine(ctx)
+
+	err := engine.LoadModule(ctx, "hello", "modules/hello.wasm")
+
+	// if there is an error loading the module, print the error and exit the program
+	if err != nil {
+		log.Fatalf("Failed to load module: ", err)
+	}
+
+	server := &worker.WorkerServer{
+		Engine: engine,
+	}
+
+	log.Println("Worker server is running on port 8080...")
+
+	err = server.Start("8080")
+	if err != nil {
+		log.Fatalf("Failed to start server: ", err)
+	}
+}
--- internal/worker/server.go
+++ internal/worker/server.go
@@ -21,8 +21,21 @@ func (s *WorkerServer) handleInvoke(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&req)
 	result, err := s.Engine.Execute(r.Context(), req.ModuleName, req.Payload)
 
+	// Error handling in case user send bad JSON / nonexistent module
+	if err != nil {
+		http.Error(w, "Bad Request: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	resp := shared.ExecutionResponse{
 		Result: result,
 	}
 	json.NewEncoder(w).Encode(resp)
+}
+
+func (s *WorkerServer) Start(port string) error {
+	// run handleInvoke when getting a request to /invoke endpoint
+	http.HandleFunc("/invoke", s.handleInvoke)
+	// start the server on the specified port and listen for requests
+	return http.ListenAndServe(":"+port, nil)
 }
