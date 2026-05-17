76ffabee88fc5b51123b89fde93252031ac204b3
sleepysaki_ <caotrankhanhlinh35@gmail.com>
2026-05-17 12:06:42 +0700

    add main.go

---
+++ cmd/master/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"time"
+	"wasmcat/internal/master"
+)
+
+func main() {
+	log.Println("Initializing Control plane...")
+
+	// Dependency Injection & Initialization
+
+	// Create the State Registry
+	// Initialize the thread-safe map (Mutex) for tracking workers.
+	reg := master.NewRegistry()
+	log.Println("State Registry initialized.")
+
+	// Create the Scheduler
+	sched := &master.Scheduler{}
+	log.Println("Spatial Scheduler initialized.")
+
+	// Create the Dispatcher, need both the Registry and the Scheduler
+	dispatch := &master.Dispatcher{
+		Registry:  reg,
+		Scheduler: sched,
+	}
+	log.Println("Execution Dispatcher initialized.")
+
+	// Create the API Gateway, need the Registry (to handle /register and /heartbeat) and the Dispatcher (to handle /api/v1/execute)
+	gateway := &master.Gateway{
+		Registry:   reg,
+		Dispatcher: dispatch,
+	}
+
+	// Background Processes
+
+	// Start the background garbage collection
+	// Run completely independently of the web server
+	// Every 15 seconds, it scrubs the Registry for dead edge nodes
+	go func() {
+		log.Println("Background Reaper started (15s interval).")
+		for {
+			time.Sleep(15 * time.Second)
+			reg.Cleanup()
+		}
+	}()
+
+	// Ignition
+
+	// Turn on the API Server
+	// This is a blocking call. The program will stay on this line forever unless the server crashes.
+	port := "8080"
+	log.Printf("Master Gateway is LIVE on port %s.\n", port)
+
+	err := gateway.Start(port)
+	if err != nil {
+		log.Fatalf("CRITICAL: Master Gateway crashed: %v", err)
+	}
+}
